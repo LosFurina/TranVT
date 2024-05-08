770fdbdead8ee5e4edb54f1e7ba5669d799b6a24
unknown <liweijun0302@gmail.com>
2024-05-08 23:19:10 +0800

    add(random train)

---
+++ random_train.py
@@ -0,0 +1,32 @@
+import subprocess
+
+from tqdm import tqdm
+
+
+class RandomTrain(object):
+
+    def __init__(self):
+        self.save_pattern = "wadi_win50_TranVTV_pre_0.8"
+        self.dataset = "wadi"
+        self.model = "TranVTV"
+        self.lr = 0.0001
+        self.win_size = 50
+        self.batch_size = 32
+        self.epochs = 10
+        self.recon = None
+        self.train_ratio = 0.8
+        self.train_times = 2
+
+    def train(self):
+        cmd = f"python train.py --save_pattern {self.save_pattern} --dataset {self.dataset} --model {self.model} --lr {self.lr} --win_size {self.win_size} --batch_size {self.batch_size} --epochs {self.epochs} --train_ratio {self.train_ratio}"
+        print("CMD:", cmd)
+
+        pbar = tqdm(range(self.train_times), desc="Training progress")
+        for i in pbar:
+            pbar.set_description(desc=f"Training iteration {i}")
+            subprocess.run(cmd, shell=True)
+
+
+if __name__ == "__main__":
+    main = RandomTrain()
+    main.train()
